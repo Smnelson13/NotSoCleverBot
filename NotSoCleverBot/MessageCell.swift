a62c59ba0f216ea933134743746afc81d18ac701
David Johnson <david.j.c.johnson@gmail.com>
2017-04-24 13:25:26 -0400

    + InsetLabel class, + implemented InsetLabel

--- NotSoCleverBot/MessageCell.swift
+++ NotSoCleverBot/MessageCell.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class MessageCell: UITableViewCell {
-  @IBOutlet weak var outputLabel: UILabel!
+  @IBOutlet weak var outputLabel: InsetLabel!
   
   override func awakeFromNib()
   {
@@ -23,3 +23,23 @@ class MessageCell: UITableViewCell {
 
 class UserMessageCell: MessageCell { }
 class BotMessageCell: MessageCell { }
+
+//http://stackoverflow.com/questions/32239457/how-do-you-add-inset-to-uilabel-ios-swift
+class InsetLabel: UILabel {
+  let topInset = CGFloat(8)
+  let bottomInset = CGFloat(8)
+  let leftInset = CGFloat(16)
+  let rightInset = CGFloat(16)
+  
+  override func drawText(in rect: CGRect) {
+    let insets: UIEdgeInsets = UIEdgeInsets(top: topInset, left: leftInset, bottom: bottomInset, right: rightInset)
+    super.drawText(in: UIEdgeInsetsInsetRect(rect, insets))
+  }
+  
+  override public var intrinsicContentSize: CGSize {
+    var intrinsicSuperViewContentSize = super.intrinsicContentSize
+    intrinsicSuperViewContentSize.height += topInset + bottomInset
+    intrinsicSuperViewContentSize.width += leftInset + rightInset
+    return intrinsicSuperViewContentSize
+  }
+}
